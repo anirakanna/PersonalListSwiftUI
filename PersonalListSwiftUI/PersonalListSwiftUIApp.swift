6662a5a94d54bb9a67dadb0e3f1fd843d4f2936e
anirakanna <evgenia.shipova@gmail.com>
2020-10-29 22:01:18 +0300

    Initial Commit

---
+++ PersonalListSwiftUI/PersonalListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalListSwiftUIApp.swift
+//  PersonalListSwiftUI
+//
+//  Created by Evgenia Shipova on 29.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct PersonalListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
